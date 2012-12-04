b624c4c1bcf1673a9054e9a1e54705c6ba16260a
Victor Zagorski <victor@zagorski.ru>
2012-12-04 16:55:46 +0800

    Updated preview display

--- app/models/spree/post.rb
+++ app/models/spree/post.rb
@@ -39,12 +39,17 @@ class Spree::Post < ActiveRecord::Base
   end
   	
 	def rendered_preview
-    preview = body.split("<!-- more -->")[0]
+    preview = body.split(/<hr[^>]+>/i).first
+
     render(preview)
   end
-	
+
+  def has_preview?
+    body =~ /<hr[^>]+>/i
+  end
+
 	def rendered_body
-	  render(body.gsub("<!-- more -->", ""))
+	  render(body.gsub(/<hr[^>]+>/i, ""))
   end
 		
 	def preview_image
