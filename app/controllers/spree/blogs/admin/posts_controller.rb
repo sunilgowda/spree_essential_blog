27003de16bae802ab428409c021ae903e14edadb
Victor Zagorski <victor@zagorski.ru>
2012-12-13 15:30:40 +0800

    Fixed admin page posts ordering

--- app/controllers/spree/blogs/admin/posts_controller.rb
+++ app/controllers/spree/blogs/admin/posts_controller.rb
@@ -1,34 +1,41 @@
 class Spree::Blogs::Admin::PostsController < Spree::Admin::ResourceController
+  prepend_before_filter :add_default_params
 
   update.before :set_category_ids
-  
+
+
   def new
     @post = Spree::Post.new
     @post.posted_at ||= Time.now
   end
 
 private
-  
+
   def set_category_ids
     if params[:post] && params[:post][:post_category_ids].is_a?(Array)
       params[:post][:post_category_ids].reject!{|i| i.to_i == 0 }
     end
   end
-  
+
   def location_after_save
     path = params[:redirect_to].to_s.strip.sub(/^\/+/, "/")
     path.blank? ? object_url : path
-  end 
-  
+  end
+
   def find_resource
   	@object ||= Spree::Post.find_by_path(params[:id])
   end
-  
+
   def collection
-    params[:search] ||= {}
-    params[:search][:meta_sort] ||= "posted_at.desc"
+    # params[:search] ||= {}
+    # params[:search][:meta_sort] ||= "posted_at desc"
     @search = Spree::Post.search(params[:q])
     @collection = @search.result.page(params[:page]).per(Spree::Config[:orders_per_page])
+  end
+
+  def add_default_params
+    params[:q] ||= {}
+    params[:q][:s] ||= 'posted_at desc'
   end
 
 end
